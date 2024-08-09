4b51384e52ad08e7becf14f5268023c813d0f082
dericholas <nickderis@gmail.com>
2024-08-09 17:06:24 -0400

    styling

--- app/component/navbar.jsx
+++ app/component/navbar.jsx
@@ -4,12 +4,24 @@ const NavBar = () => {
 
     return (
         <div className="navbar">
-            <img className="navbar__home"/>
-            <ul className="navbar__menu">
-                <li> <Link href="/about" className="navbar__menu__link">ABOUT</Link> </li>
-                <li> <Link href="/projects" className="navbar__menu__link">PORTFOLIO</Link> </li>
-                <li> <Link href="/contact" className="navbar__menu__link">CONTACT</Link> </li>
-            </ul>
+            <div className="navbar__left">
+            <a href="/"className="navbar__left__logo">
+                    Home
+                </a>
+            </div>
+            <div className="navbar__center">
+                <ul className="navbar__center__socials">
+                    <li> <a href="https://github.com/dericholas" className="navbar__center__socials__icon" target="_blank">GH</a> </li>
+                    <li> <a className="navbar__center__socials__icon" href="https://www.linkedin.com/in/nick-deris/" target="_blank">Linkdin</a> </li>
+                </ul>
+            </div>
+            <div className="navbar__right">
+                <ul className="navbar__right__menu">
+                    <li> <a href="/about" className="navbar__right__menu__link">ABOUT</a> </li>
+                    <li> <a href="/projects" className="navbar__right__menu__link">PORTFOLIO</a> </li>
+                    <li> <a href="/contact" className="navbar__right__menu__link">CONTACT</a> </li>
+                </ul>
+            </div>
         </div>
     )
 }
